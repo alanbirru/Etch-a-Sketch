91a04b1c2ac06b413787b4b70027976bb8fe12cc
alan birrueta <alanb_99@hotmail.com>
2022-02-01 15:35:29 -0700

    finished

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 let container = document.querySelector("#container");
-let size = 16;
+let eraseButton = document.querySelector(".eraser");
+let rgbButton = document.querySelector(".rgb");
+let clearButton = document.querySelector(".clear");
+let sizeButton = document.querySelector(".size");
+let blackButton = document.querySelector(".black");
+let size = prompt("Choose grid size between 1-100");
 let color = "black";
+
+if (size > 100 || size <= 0) {
+  size = alert("invalid size try again!");
+  window.location.reload();
+}
 
 function makeGrid() {
   // container.style.border = "solid 5px black";
@@ -15,21 +25,26 @@ function makeGrid() {
     container.appendChild(rows);
   }
 }
+
+// aparece el grid
 makeGrid();
 
 let squares = document.querySelectorAll(".squares");
 
-for (let i = 0; i < squares.length; i++) {
-  squares[i].style.width = `${500 / size}px`;
-  squares[i].style.height = `${500 / size}px`;
-  squares[i].addEventListener("mouseover", function () {
-    if (color === "random") {
-      let radomColor = getRandomColor();
-      squares[i].style.backgroundColor = radomColor;
-    }
-    squares[i].style.backgroundColor = color;
-  });
+function hovering(size) {
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.width = `${500 / size}px`;
+    squares[i].style.height = `${500 / size}px`;
+    squares[i].addEventListener("mouseover", function () {
+      if (color === "rgb") {
+        let radomColor = getRandomColor();
+        squares[i].style.backgroundColor = radomColor;
+      }
+      squares[i].style.backgroundColor = color;
+    });
+  }
 }
+hovering(size);
 
 function getRandomColor() {
   var letters = "0123456789ABCDEF";
@@ -39,3 +54,30 @@ function getRandomColor() {
   }
   return color;
 }
+
+function clear() {
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = "white";
+  }
+}
+
+rgbButton.addEventListener("click", function () {
+  color = "rgb";
+});
+
+clearButton.addEventListener("click", clear);
+
+sizeButton.addEventListener("click", function () {
+  window.location.reload();
+  if (size > 100) {
+    size = prompt("invalid");
+  }
+});
+
+eraseButton.addEventListener("click", function () {
+  color = "white";
+});
+
+blackButton.addEventListener("click", function () {
+  color = "black";
+});
